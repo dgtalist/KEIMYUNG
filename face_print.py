6a9b8decaa5c64af31aff9512837ce1415ce7474
dgtalist <31330881+dgtalist@users.noreply.github.com>
2021-02-02 15:54:05 +0900

    Create face_print.py

---
+++ face_print.py
@@ -0,0 +1,24 @@
+
+from pycm import *
+
+console(BLE)
+
+rpi.mode(2)
+
+center_x=int(rpi.resolution_w()/2)
+
+while True:
+        DXL(5).goal_position(2500)
+        x=rpi.position_x()
+    if x>0:
+        if x>center_x: 
+                diff=x-center_x
+                print('[|',diff,']',sep='')
+        else:
+                diff=center_x-x
+                print('[',diff,'|]',sep='')
+
+        buzzer.note(20,100)
+        buzzer.wait()
+
+    delay(200)
